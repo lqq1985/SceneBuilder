2b3f37b23b1980fab808bac6767984dd9162a352
Mishal Zaman <mishalzaman@gmail.com>
2020-01-18 13:14:41 -0500

    update mesh

--- SceneBuilder/Model.cpp
+++ SceneBuilder/Model.cpp
@@ -26,59 +26,11 @@ void Model::loadModel(std::string filename)
     }
 }
 
-void Model::draw(glm::mat4& projection, glm::mat4& view, Shader& shader)
-{
-    shader.use();
-    shader.setMat4("projection", projection);
-    shader.setMat4("view", view);
-
-    for (unsigned int i = 0; i < meshes.size(); i++) {
-        // if (i == 0) continue;
-
-        glm::mat4 model = glm::mat4(1.0f);
-        //model = glm::translate(model, meshes[i].origin); // translate it down so it's at the center of the scene
-
-        shader.setMat4("model", model);
-
-        unsigned int diffuseNr = 1;
-        unsigned int specularNr = 1;
-        unsigned int normalNr = 1;
-        unsigned int heightNr = 1;
-        for (unsigned int j = 0; j < meshes[i].textures.size(); j++)
-        {
-            glActiveTexture(GL_TEXTURE0 + j); // active proper texture unit before binding
-            // retrieve texture number (the N in diffuse_textureN)
-            std::string number;
-            std::string name = meshes[i].textures[j].type;
-            if (name == "texture_diffuse")
-                number = std::to_string(diffuseNr++);
-            else if (name == "texture_specular")
-                number = std::to_string(specularNr++); // transfer unsigned int to stream
-            else if (name == "texture_normal")
-                number = std::to_string(normalNr++); // transfer unsigned int to stream
-            else if (name == "texture_height")
-                number = std::to_string(heightNr++); // transfer unsigned int to stream
-
-            // now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), j);
-            // and finally bind the texture
-            glBindTexture(GL_TEXTURE_2D, meshes[i].textures[j].id);
-        }
-
-        // draw mesh
-        glBindVertexArray(meshes[i].VAO);
-        glDrawElements(GL_TRIANGLES, meshes[i].indices.size(), GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        // always good practice to set everything back to defaults once configured.
-        glActiveTexture(GL_TEXTURE0);
-    }
-}
-
 void Model::processNode(aiNode* node, const aiScene* scene, aiMatrix4x4 transformation)
 {
+    // only apply transformation on meshs not entities such as lights or camera.
     transformation *= node->mTransformation;
-
+ 
     for (unsigned int i = 0; i < node->mNumMeshes; i++) {
         aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
         this->meshes.push_back(processMesh(mesh, scene, transformation));
@@ -263,10 +215,58 @@ std::vector<Texture> Model::loadMaterialTextures(aiMaterial* mat, aiTextureType 
     return textures;
 }
 
+void Model::draw(glm::mat4& projection, glm::mat4& view, Shader& shader)
+{
+    shader.use();
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
+
+    for (unsigned int i = 0; i < meshes.size(); i++) {
+        // if (i == 0) continue;
+
+        glm::mat4 model = glm::mat4(1.0f);
+        //model = glm::translate(model, meshes[i].origin); // translate it down so it's at the center of the scene
+
+        shader.setMat4("model", model);
+
+        unsigned int diffuseNr = 1;
+        unsigned int specularNr = 1;
+        unsigned int normalNr = 1;
+        unsigned int heightNr = 1;
+        for (unsigned int j = 0; j < meshes[i].textures.size(); j++)
+        {
+            glActiveTexture(GL_TEXTURE0 + j); // active proper texture unit before binding
+            // retrieve texture number (the N in diffuse_textureN)
+            std::string number;
+            std::string name = meshes[i].textures[j].type;
+            if (name == "texture_diffuse")
+                number = std::to_string(diffuseNr++);
+            else if (name == "texture_specular")
+                number = std::to_string(specularNr++); // transfer unsigned int to stream
+            else if (name == "texture_normal")
+                number = std::to_string(normalNr++); // transfer unsigned int to stream
+            else if (name == "texture_height")
+                number = std::to_string(heightNr++); // transfer unsigned int to stream
+
+            // now set the sampler to the correct texture unit
+            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), j);
+            // and finally bind the texture
+            glBindTexture(GL_TEXTURE_2D, meshes[i].textures[j].id);
+        }
+
+        // draw mesh
+        glBindVertexArray(meshes[i].VAO);
+        glDrawElements(GL_TRIANGLES, meshes[i].indices.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // always good practice to set everything back to defaults once configured.
+        glActiveTexture(GL_TEXTURE0);
+    }
+}
+
 glm::mat4 Model::aiMatrix4x4ToGlm(aiMatrix4x4 from)
 {
     glm::mat4 to;
-
 
     to[0][0] = (GLfloat)from.a1; to[0][1] = (GLfloat)from.b1;  to[0][2] = (GLfloat)from.c1; to[0][3] = (GLfloat)from.d1;
     to[1][0] = (GLfloat)from.a2; to[1][1] = (GLfloat)from.b2;  to[1][2] = (GLfloat)from.c2; to[1][3] = (GLfloat)from.d2;
